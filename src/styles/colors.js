91b2a618b7a28570d5991968d36c212af306dc7a
Uziel Patron <uzielpatron2020@gmail.com>
2022-08-29 20:53:07 -0300

    colors configured

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -1,11 +1,43 @@
 const colors = {
-  primary: "#0098d3",
-  dark: "#000",
+  primary: "#42CAC1",
+  primary_active: '#129A91',
+  dark: "#040403",
+  danger: '#d04',
+  danger_active: '#A01',
+  blue: '#3C91E6',
+  blue_active: '#0C61B6',
+  green: '#57C011',
+  green_active: '#279000',
+  violet: '#CC00CC',
+  grey: '#E8E8EA',
   // Fonts
+  fontGrey: '#666666',
   fontLight: "#fff",
   // Background,
   bgLight: "#fff",
-  bgDark: "#16222b",
+  bgDark: "#42CAC1",
 };
 
 export default colors;
+
+
+/*
+  --color-primary: #42CAC1;
+  --color-primary_hover: #32BAB1;
+  --color-primary_active: #129A91;
+  --color-dark: #040403;
+  --color-danger: #d04;
+  --color-danger_hover: #c03;
+  --color-danger_active: #A01;
+  --color-blue: #3C91E6;
+  --color-blue_hover: #2C81D6;
+  --color-blue_active: #0C61B6;
+  --color-green: #57C011;
+  --color-green_hover: #47B001;
+  --color-green_active: #279000;
+  --color-violet: #CC00CC;
+  --color-orange: #EE880A;
+  --color-fontLight: #666666;
+  --color-white: #FFFFFF;
+  --color-grey: #E8E8EA;
+*/
