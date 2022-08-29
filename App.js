7044cdfc4d209461ffbe050ce5fe3544dac44e83
Uziel Patron <uzielpatron2020@gmail.com>
2022-08-29 20:29:52 -0300

    app running

--- App.js
+++ App.js
@@ -1,20 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import "./ignoreWarnings";
+import React, { useState, useEffect, useMemo } from "react";
+import { Provider as PaperProvider } from "react-native-paper";
+import jwtDecode from "jwt-decode";
+import AuthScreen from "./src/screens/AuthScreen";
+import UserNavigation from "./src/navigation/UserNavigation";
+import { getTokenApi, setTokenApi, removeTokenApi } from "./src/api/token";
+import AuthContext from "./src/context/AuthContext";
 
 export default function App() {
+  const [auth, setAuth] = useState(undefined);
+
+  useEffect(() => {
+    (async () => {
+      const token = await getTokenApi();
+      if (token) {
+        setAuth({
+          token,
+          idUser: jwtDecode(token).id,
+        });
+      } else {
+        setAuth(null);
+      }
+    })();
+  }, []);
+
+  const login = (user) => {
+    setTokenApi(user.jwt);
+    setAuth({
+      token: user.jwt,
+      idUser: jwtDecode(user.jwt).id,
+    });
+  };
+
+  const logout = () => {
+    if (auth) {
+      removeTokenApi();
+      setAuth(null);
+    }
+  };
+
+  const authData = useMemo(
+    () => ({
+      auth,
+      login,
+      logout,
+      setReloadUser: null,
+    }),
+    [auth]
+  );
+
+  if (auth === undefined) return null;
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <AuthContext.Provider value={authData}>
+      <PaperProvider>
+        {auth ? <UserNavigation /> : <AuthScreen />}
+      </PaperProvider>
+    </AuthContext.Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/utils/constants.js
@@ -0,0 +1,6 @@
+export const API_URL = "https://fresh-eccommerce-backend-production.up.railway.app";
+// export const API_URL = "https://react-native-ecommerce-dev.herokuapp.com";
+export const TOKEN = "token";
+export const SEARCH_HISTORY = "search-history";
+export const CART = "cart";
+export const STRAPI_PUBLISHABLE_KEY ="pk_test_51IGQJXFotiOk2WnmTOM6HpcPnfjZSX98o9FqA5RJiMKB6jm62hiVu7mtk9rxgMjPSbjdIJxGmN6jwFNQ69IQ7f9O00AthydrqM"
